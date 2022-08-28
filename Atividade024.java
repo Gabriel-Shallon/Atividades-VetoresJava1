88c4e91126be15a8559bcb3f33d50fcde5d2e0e7
Gabriel-Shallon <shallonhatake@gmail.com>
2022-08-28 15:16:17 -0400

    Update Atividade024.java

--- Atividade024.java
+++ Atividade024.java
@@ -13,11 +13,28 @@ public class Atividade024 {
                 int [] vetA = new int[10];
                 int [] vetB = new int[5];
          
-         for (int i = 0; i < vetA.length; i++){
-         
-         vetA[i] = Integer.parseInt(JOptionPane.showInputDialog(null, "Entre com os elementos do vetor A na posição ["+(i+1)+"]."));
-         
-         }
+                UIManager.put("OptionPane.yesButtonText","Números aleatórios");
+                UIManager.put("OptionPane.noButtonText","Números selecionados");
+                
+                int b = JOptionPane.showConfirmDialog(null, "Deseja gerar números aleatórios (de 0 a 10) para o vetor A ou seleciona-los?", "Escolher.Vetor", JOptionPane.YES_NO_OPTION);
+                
+                   if (b == 0){
+       
+                       for (int i = 0; i < vetA.length; i++){
+       
+                       vetA[i] = (int)Math.round(Math.random() * 10); 
+       
+                       }
+                   }
+       
+                if (b == 1){
+       
+                for (int i = 0; i < vetA.length; i++){
+                
+                vetA[i] = Integer.parseInt(JOptionPane.showInputDialog(null, "Entre com os elementos do vetor A na posição ["+(i+1)+"]."));
+               
+                }
+               }
 
          for (int i = 0; i < vetA.length;i++){
 
