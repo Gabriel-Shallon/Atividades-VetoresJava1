126bb815c29933eeda230bc88030954d9708b9ce
Gabriel-Shallon <shallonhatake@gmail.com>
2022-08-29 10:02:06 -0400

    Update Atividade020e21.java

--- Atividade020e21.java
+++ Atividade020e21.java
@@ -1,11 +1,40 @@
 import javax.swing.JOptionPane;
+import javax.swing.UIManager;
 
 
 public class Atividade020e21 {
     
     public static void main(String[] args){
 
-JOptionPane.showMessageDialog(null, "Não entendi oque o enunciado dessa queria dizer :(");
-          
+               
+                
+        int A = 0; 
+        
+        while (A!=1){      
+              double [] vetA= new double[20];
+              
+        
+            double cotacao = Double.parseDouble(JOptionPane.showInputDialog(null,"Informe quantos reais um dolar está valendo no momento."));
+             
+        
+              for (int i=0; i < vetA.length; i++) {
+                vetA[i] = cotacao * (i+1);
+              }
+        
+             
+              
+              for (int i = 0; i < vetA.length; i++) {
+                JOptionPane.showMessageDialog(null, (i+1)+" Dolars  vale: "+vetA[i]+" reais.");
+              } 
+              
+              
+
+              UIManager.put("OptionPane.yesButtonText","Sim");
+              UIManager.put("OptionPane.noButtonText","Não");
+              
+              
+                A = JOptionPane.showConfirmDialog(null, "Deseja executar novamente?","Reiniciar", JOptionPane.YES_NO_OPTION); 
+                
+            }
         }
-    }
+    }   
